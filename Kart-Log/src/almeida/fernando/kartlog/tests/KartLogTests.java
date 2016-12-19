26c67ee98e6078b4167e331c8821f59d00e7d8fd
Fernando Almeida <almeidascvn@gmail.com>
2016-12-19 14:40:51 -0200

    Simple Unit Tests

--- Kart-Log/src/almeida/fernando/kartlog/tests/KartLogTests.java
+++ Kart-Log/src/almeida/fernando/kartlog/tests/KartLogTests.java
@@ -1,5 +1,50 @@
 package almeida.fernando.kartlog.tests;
 
-public class KartLogTests {
+import java.text.DecimalFormat;
+import java.util.Map;
+
+import org.junit.Test;
+
+import almeida.fernando.kartlog.model.KartDriver;
+import almeida.fernando.kartlog.reader.KartLogReader;
+import junit.framework.TestCase;
+
+public class KartLogTests extends TestCase {
+	
+	KartLogReader reader = new KartLogReader();
+	Map<Integer, KartDriver> raceLog;
+	
+	@Override
+	protected void setUp() throws Exception {
+		raceLog = reader.read("corrida_test.log");
+	}
+	
+	//F.MASSA NAME
+	@Test
+	public void testName(){
+		KartDriver driver = raceLog.get(38);
+		assertEquals("F.MASSA" , driver.getName());
+	}
+	
+	//RAIKKONEN AVG SPEED
+	@Test
+	public void testAvgSpeed(){
+		KartDriver driver = raceLog.get(2);
+		DecimalFormat df = new DecimalFormat("#.00"); 
+		
+		assertEquals("43,63", df.format(driver.getRaceAvgSpeed()));
+	}
+	
+	//VETTEL BEST LAP
+	@Test
+	public void testBestLap(){
+		KartDriver driver = raceLog.get(11);
+		assertEquals(new Integer(3) , driver.getBestLap().getLapNumber());
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		raceLog = null;
+	}
 
 }
